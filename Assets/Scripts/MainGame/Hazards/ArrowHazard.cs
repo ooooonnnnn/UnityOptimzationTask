91ad266aed3771f7b98eefdae2af9521c363df78
ilayabr <ilayabr@gmail.com>
2026-02-23 15:34:46 +0200

    arrow hazard refactor

--- Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
+++ Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
@@ -1,20 +1,36 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.Serialization;
 
 public class ArrowHazard : MonoBehaviour
 {
-    public GameObject arrowPrefab;
-    [SerializeField] float shootInterval;
+    //public GameObject arrowPrefab;
+    private static ObjectPool<ArrowObject> arrowPool;
+    [SerializeField] private float arrowTimeAlive;
+    [SerializeField] private float shootInterval;
     private float shootIntervalLeft;
-    
-    private void Awake()
-    {
-        
-    }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
+        if (arrowPool == null)
+        {
+            arrowPool = new ObjectPool<ArrowObject>(() =>
+            {
+                ArrowObject arrow = Instantiate(Resources.Load<ArrowObject>("ArrowObject"));
+                return arrow;
+            }, arrow =>
+            {
+                arrow.gameObject.SetActive(true);
+            }, arrow =>
+            {
+                arrow.gameObject.SetActive(false);
+            }, arrow =>
+            {
+                Destroy(arrow.gameObject);
+            }, false, 10, 100);
+        }
         shootIntervalLeft = shootInterval;
     }
 
@@ -24,10 +40,20 @@ public class ArrowHazard : MonoBehaviour
         shootIntervalLeft -= Time.deltaTime;
         if (shootIntervalLeft <= 0)
         {
-            ArrowObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity).GetComponent<ArrowObject>();
-            arrow.transform.Rotate(0,90,0);
-            arrow.transform.Rotate(0,90,0);
+            ArrowObject arrow = arrowPool.Get();
+            arrow.transform.position = transform.position;
+            arrow.transform.rotation = Quaternion.Euler(0, 180, 0);
+            //ArrowObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity).GetComponent<ArrowObject>();
+            //arrow.transform.Rotate(0,90,0);
+            //arrow.transform.Rotate(0,90,0);
             shootIntervalLeft = shootInterval;
+            StartCoroutine(ReturnAfter(arrow, 3f));
         }
+    }
+    
+    private static IEnumerator ReturnAfter(ArrowObject arrow, float time)
+    {
+        yield return new WaitForSeconds(time);
+        arrowPool.Release(arrow);
     }
 }
